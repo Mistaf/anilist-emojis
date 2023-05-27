4cb9b6f106487917087deb955ff06c85bf976e7c
Mistaf <21952929+Mistaf@users.noreply.github.com>
2023-05-27 05:39:15 +0200

    Added replace emoji button
    So you can be certain the script works correctly by pressing the button

--- EmojiReplacer.user.js
+++ EmojiReplacer.user.js
@@ -3,50 +3,75 @@
 // @namespace   Violentmonkey Scripts
 // @match       https://anilist.co/*
 // @grant       none
-// @version     1.0
+// @version     1.1
 // @author      - Mistaf
 // @description replaces emojis in messages with the HTML entities to be displayed on anilist as emoji
 // @downloadURL https://raw.githubusercontent.com/Mistaf/anilist-emojis/main/EmojiReplacer.js
 // @updateURL   https://raw.githubusercontent.com/Mistaf/anilist-emojis/main/EmojiReplacer.js
 // ==/UserScript==
 
-const targetNode = document.querySelector("div#app");
 const config = { childList: true, subtree: true };
 
 const callback = (mutationList, observer) => {
     for (const mutation of mutationList) {
         if (
-            mutation.target.className == "activity-edit" &&
-            Array.from(mutation.addedNodes).find(
-                (node) => node.className == "actions"
-            )
+            mutation.target.className == "activity-edit" ||
+            mutation.target.className == "comment-editor active"
         ) {
+            const textArea = mutation.target.querySelector("textarea");
             const actions = Array.from(mutation.addedNodes).find(
                 (node) => node.className == "actions"
             );
-            const saveButtons = actions.querySelectorAll("div.button.save");
-            saveButtons.forEach((saveButton) => {
-                saveButton.addEventListener("click", () => {
+
+            const markdownEditor = mutation.target.querySelector(
+                "div.markdown-editor"
+            );
+            if (
+                markdownEditor &&
+                markdownEditor.lastChild.className != "emojiReplacer"
+            ) {
+                let emojiButton = document.createElement("div");
+                emojiButton.className = "emojiReplacer";
+                emojiButton.append("😀");
+
+                emojiButton.onclick = (event) => {
                     const textArea =
-                        saveButton.parentElement.parentElement.querySelector(
-                            "textarea"
-                        );
-                    textArea.value
-                        .match(
-                            /(\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])/g
-                        )
-                        .forEach((match) => {
-                            textArea.value = textArea.value.replace(
-                                match,
-                                `&#${match.codePointAt()};`
-                            );
-                            console.log(match, `$#${match.codePointAt()};`);
-                        });
+                        markdownEditor.parentElement.querySelector("textarea");
+                    replaceEmojis(textArea);
+                };
+                markdownEditor.append(emojiButton);
+            }
+            if (actions) {
+                const saveButtons = actions.querySelectorAll("div.button.save");
+                saveButtons.forEach((saveButton) => {
+                    saveButton.addEventListener("click", () => {
+                        replaceEmojis(textArea);
+                    });
                 });
-            });
+            }
         }
     }
 };
 
+function replaceEmojis(textArea) {
+    textArea.value
+        .match(
+            /(\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])/g
+        )
+        .forEach((match) => {
+            textArea.value = textArea.value.replace(
+                match,
+                `&#${match.codePointAt()};`
+            );
+        });
+}
+
 const observer = new MutationObserver(callback);
-observer.observe(targetNode, config);
+
+const interval = setInterval(() => {
+    const targetNode = document.querySelector("div#app");
+    if (targetNode && targetNode.children.length > 0) {
+        observer.observe(targetNode, config);
+        clearInterval(interval);
+    }
+}, 100);
